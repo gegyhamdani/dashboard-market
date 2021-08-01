507a75a6baebb2cc13a8888aa86f5fbf7eac748e
gegyhamdani <gegyhamdani@gmail.com>
2021-08-01 21:45:17 +0700

    Create dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import PageLayout from "./component/PageLayout";
 import HeaderLayout from "./component/HeaderLayout";
 import SiderLayout from "./component/SiderLayout";
+import Dashboard from "./component/Dashboard";
 
 import "./App.css";
 
 const App = () => (
   <div className="container">
     <PageLayout headerChildren={<HeaderLayout />} siderChildren={<SiderLayout />}>
-      Content
+      <>
+        <Dashboard />
+      </>
     </PageLayout>
   </div>
 );
---
+++ src/component/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import styles from "./index.module.css";
+
+const Dashboard = () => (
+  <div className={styles.container}>
+    <h1 className={styles.title}>Dashboard</h1>
+  </div>
+);
+
+export default Dashboard;
