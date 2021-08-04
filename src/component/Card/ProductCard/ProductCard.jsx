d9a5193710bb0667f982733606c9018488da0253
gegyhamdani <gegyhamdani@gmail.com>
2021-08-04 21:54:16 +0700

    create product card

---
+++ src/component/Card/ProductCard/ProductCard.jsx
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types";
+
+import BaseCard from "../BaseCard";
+
+import styles from "./index.module.css";
+
+const ProductCard = ({ highlight }) => (
+  <BaseCard className={`${styles.card} ${highlight && styles.highlight}`}>
+    <div className={styles.container}>
+      <img
+        src="./assets/icon/sales-icon.png"
+        alt="product"
+        className={`${styles["product-image"]} ${highlight && styles["highlight-image"]}`}
+      />
+      <div className={`${styles.product} ${highlight && styles["highlight-product"]}`}>
+        <h5>Nama Produk</h5>
+        <div className={`${styles["product-detail"]} ${highlight && styles["highlight-product-detail"]}`}>
+          <p>Rp 100.000</p>
+          <p>100</p>
+        </div>
+      </div>
+    </div>
+  </BaseCard>
+);
+
+ProductCard.propTypes = {
+  highlight: PropTypes.bool
+};
+
+ProductCard.defaultProps = {
+  highlight: false
+};
+
+export default ProductCard;
--- src/component/Card/SKUCard/SKUCard.jsx
+++ src/component/Card/SKUCard/SKUCard.jsx
@@ -2,6 +2,7 @@ import { MoreOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
 
 import BaseCard from "../BaseCard";
+import ProductCard from "../ProductCard";
 
 import styles from "./index.module.css";
 
@@ -9,9 +10,18 @@ const SKUCard = ({ title }) => (
   <BaseCard className={styles.card} isShowBorder isShowBoxShadow>
     <div className={styles["card--header"]}>
       <h4>{title}</h4>
-      <MoreOutlined style={{ fontSize: "20px" }} />
+      <div className={styles["card--wrapper__icon"]}>
+        <MoreOutlined style={{ fontSize: "20px" }} />
+      </div>
     </div>
-    <div className={styles["card--body"]} />
+    <div className={styles["card--body"]}>
+      <ProductCard highlight />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
   </BaseCard>
 );
 
