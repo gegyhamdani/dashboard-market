66478457d84e05961731ca772fd53dd83bacc1f7
gegyhamdani <gegyhamdani@gmail.com>
2021-08-06 13:23:41 +0700

    add dropdown to chart

--- src/component/Card/ChartCard/ChartCard.jsx
+++ src/component/Card/ChartCard/ChartCard.jsx
@@ -1,3 +1,5 @@
+import { Select } from "antd";
+
 import { MoreOutlined } from "@ant-design/icons";
 
 import Chart from "../../Chart";
@@ -5,12 +7,21 @@ import BaseCard from "../BaseCard";
 
 import styles from "./index.module.css";
 
+const { Option } = Select;
+
 const ChartCard = () => (
   <BaseCard className={styles.card}>
     <div className={styles["card--header"]}>
       <h4>AVERAGE PURCHASE VALUE</h4>
-      <div className={styles["card--wrapper__icon"]}>
-        <MoreOutlined style={{ fontSize: "20px" }} />
+      <div className={styles["card--wrapper__action"]}>
+        <Select defaultValue="sixmonth" style={{ width: 150, marginRight: "5px" }}>
+          <Option value="sixmonth">Last 6 months</Option>
+          <Option value="lastmonth">Last months</Option>
+          <Option value="lastweeks">Last weeks</Option>
+        </Select>
+        <div className={styles["card--wrapper__icon"]}>
+          <MoreOutlined style={{ fontSize: "20px" }} />
+        </div>
       </div>
     </div>
     <div className={styles["card--body"]}>
